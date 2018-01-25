d6ebc9e01c8674228e2af1fecbbb0e45b92573d1
Алексей <tokalig@mail.ru>
2018-01-25 22:31:46 +0300

    #6953646 - Реализован функционал тетового заполнения таблиц в БД

--- controllers/test.js
+++ controllers/test.js
@@ -1,10 +1,39 @@
 const {User, Role, Basket, Good, Content} = require('../db');
 
+// Добавление тестового пользователя в таблицу
 User.create({
-    name: 'Алексей',
-    surname: 'Иванов',
-    email: 'trololo@gmail.com',
-    login: 'myLogin',
-    password: 'myPassword',
+    name: 'testName',
+    surname: 'testSurname',
+    email: 'test@gmail.com',
+    login: 'testLogin',
+    password: 'testPassword',
     role: 1
+});
+
+// Добавление тестовой роли пользователя в таблицу
+Role.create({
+    name: 'testRole'
+});
+
+// Добавление тестового продукта в таблицу
+Good.create({
+    name: 'testName',
+    ram: 4,
+    memory: 500,
+    color: 'testColor',
+    price: 3000,
+    typeOfGood: 'testLaptop',
+    count: 10
+});
+
+// Заполнение корзины тестовыми данными
+Basket.create({
+    good: 1,
+    count: 1,
+    user: 1
+});
+
+// Добавление тестового контента в таблицу
+Content.create({
+    text: 'testContent'
 });
